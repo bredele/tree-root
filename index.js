b61070a332d5812affb96d33935ddab927e56ea7
bredele <olivier.wietrich@gmail.com>
2017-05-07 17:23:09 -0600

    Add code documentation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 
-module.exports = function(ids, index) {
-  while (index !== ids[index]) index = ids[index]
+/**
+ * Return the root index of a node in an array representation
+ * of a tree of nodes.
+ *
+ * @param {Array} arr
+ * @param {Number} index
+ * @api public
+ */
+
+module.exports = function(arr, index) {
+  while (index !== arr[index]) index = arr[index]
   return index
 }
